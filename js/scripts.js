b02a2edbb4793e765a7564ae1be37004185243b3
savsalmon <savannah.b.hilton@gmail.com>
2023-09-04 07:14:41 -0600

    added scroll button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+/* NAV open and close */
 /* Open */
 function openNav() {
   document.getElementById("myNav").style.width = "100%"
@@ -8,21 +9,25 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%"
 }
 
-/*const menu = document.querySelector(".menu")
-const menuText = document.querySelectorAll("p .menuText")
-const closeIcon = document.querySelector("#closeIcon")
-const menuBtn = document.querySelector(".menuBtn")
+/* SCROLL UP to top button code */
+// Get the button:
+let mybutton = document.getElementById("myBtn")
 
-function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu")
-    closeIcon.style.display = "none"
-    menuText.style.display = "block"
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction()
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block"
   } else {
-    menu.classList.add("showMenu")
-    closeIcon.style.display = "block"
-    menuText.style.display = "none"
+    mybutton.style.display = "none"
   }
 }
 
-menuBtn.addEventListener("click", toggleMenu)*/
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0 // For Safari
+  document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+}
